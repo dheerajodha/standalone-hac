7b0786acee32a8c3c7c196d5b11cc1aae6a9f7c2
Dheeraj Singh Jodha <dheerajsinghjodha@yahoo.com.au>
2023-08-02 15:18:39 +0530

    update to latest hac image

--- hack/install_hac.sh
+++ hack/install_hac.sh
@@ -41,6 +41,6 @@ yq '.spec.source.path="'$REPO_PATH'"' $ROOT/argo-cd-apps/app-of-apps/all-applica
 
 kubectl create secret docker-registry redhat-appstudio-staginguser-pull-secret --from-file=.dockerconfigjson="$ROOT/hack/nocommit/quay-io-auth.json" --dry-run=client -o yaml | \
 kubectl apply -f - -n user1-tenant
-oc secrets link pipeline redhat-appstudio-staginguser-pull-secret --for=pull,mount
+oc secrets link appstudio-pipeline redhat-appstudio-staginguser-pull-secret --for=pull,mount
 # switch to the correct single namespace 
 oc project user1-tenant
